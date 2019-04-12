b3eec119b57ba76cbc74c34bcc222752e16a14e0
Abduvakilov <abd_dilshod@mail.ru>
2019-04-12 21:47:10 +0500

    Update dht22.c

--- dht22.c
+++ dht22.c
@@ -140,8 +140,15 @@ int main(int argc, char** argv)
 				celsius *= -1;
 			}
 
-			// Display all data
-			printf("%6.2f \n%6.2f \n\n", celsius, humidity);
+			if(argc > 2)
+			{
+				if(argv[2]=="hum") printf("%6.2f", humidity);
+				if(argv[2]=="temp") printf("%6.2f", humidity);
+			} else
+			{
+				// Display all data
+				printf("%6.2f \n%6.2f \n", celsius, humidity);
+			}
 			
 			return 0;
 		}
